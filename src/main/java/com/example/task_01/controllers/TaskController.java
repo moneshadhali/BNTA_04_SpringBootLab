cc8292406ff58385a432d2f2bd2de32d520b49b6
Monesha <moneshadhali@gmail.com>
2024-02-22 16:10:05 +0000

    completed extendion tasks

--- src/main/java/com/example/task_01/controllers/TaskController.java
+++ src/main/java/com/example/task_01/controllers/TaskController.java
@@ -1,16 +1,39 @@
 package com.example.task_01.controllers;
 
+import com.example.task_01.models.Celebration;
 import com.example.task_01.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/greeting")
 public class TaskController {
 
     @GetMapping
-    public Greeting task(){
-        return new Greeting("Colin", "5pm");
+    public Greeting task(@RequestParam(required = false) String timeOfDay){
+        return new Greeting("Colin", timeOfDay);
     }
+
+    //URL USED:
+    //http://localhost:8080/greeting?timeOfDay=10pm
+    //http://localhost:8080/greeting
+
+    @GetMapping("/summer")
+    public Celebration task2(){
+        return new Celebration("Happy summer solstice!");
+    }
+
+    //URL USED:
+    //http://localhost:8080/greeting/summer
+
+//    Optional way to do the same thing:
+//    public String task(){
+//        Greeting greet = new Greeting("Colin", "5pm");
+//        return "Hello "+greet.getName()+" ! Its "+greet.getTimeOfDay();
+//    }
+
 }
---
+++ src/main/java/com/example/task_01/models/Celebration.java
@@ -0,0 +1,20 @@
+package com.example.task_01.models;
+
+public class Celebration {
+    private String message;
+
+    public Celebration(){
+
+    }
+    public Celebration(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

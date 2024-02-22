384482472eaefc9889c6ba75dbe87ef90c5c8dfe
Monesha <moneshadhali@gmail.com>
2024-02-22 15:27:01 +0000

    completed MVP tasks

---
+++ src/main/java/com/example/task_01/controllers/TaskController.java
@@ -0,0 +1,16 @@
+package com.example.task_01.controllers;
+
+import com.example.task_01.models.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greeting")
+public class TaskController {
+
+    @GetMapping
+    public Greeting task(){
+        return new Greeting("Colin", "5pm");
+    }
+}
